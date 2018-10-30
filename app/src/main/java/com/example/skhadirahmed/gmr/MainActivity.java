7474fed1843460e0d1464a6f7cdbc6bc84266157
syed khadir ahmed <skhadirahmed@gmail.com>
2018-10-30 19:28:34 +0530

    Added the dependencies for firebase

--- app/src/main/java/com/example/skhadirahmed/gmr/MainActivity.java
+++ app/src/main/java/com/example/skhadirahmed/gmr/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.skhadirahmed.gmr;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGNIN = 904;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    //user is already signed in
+                    Toast.makeText(MainActivity.this, "You are already signed in.", Toast.LENGTH_SHORT).show();
+                } else {
+                    //user is not signed in so give the option to sign in
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build())
+                                    ).build(), RC_SIGNIN);
+                }
+            }
+        };
     }
 }
